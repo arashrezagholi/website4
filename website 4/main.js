6597bf3cda786ce38f285951e9b684e19996a521
arashrezagholi <109976731+arashrezagholi@users.noreply.github.com>
2022-07-25 20:25:35 +0430

    Add files via upload

---
+++ website 4/main.js
@@ -0,0 +1,31 @@
+// console.log("JJF")
+// console.log("JJF")console.log(google)
+// console.log("JJF")console.log(google)
+// console.log("JJF")console.log(google)
+// console.log("JJF");
+// console.log("google");
+// const student = [12 ,15, 20];
+
+
+// const radius = 6;
+// let p = 2*Math.PI*radius;
+// p = Math.round(p);
+// console.log(p);  
+
+// let s = Math.PI * Math.pow(radius,2);
+// s = Math.round(s);
+// console.log(s);
+// let name = "hello Milad";
+// let name1 = name.split(" ");
+// console.log(name1)
+// // console.log("your name is "+name1[1])
+// const na = `your name is ${name1[1]}`
+// console.log(na)
+// let num = 2;
+// console.log(num**3)
+
+
+// const name = [["ali","reza"],["thomas","david"]];
+//  let newArray = [...name[0],...name[1]];
+//  newArray = newArray.sort();
+//  console.log(newArray[1]);
